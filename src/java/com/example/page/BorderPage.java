71655c2592fdb356fcc167e280c70cda9389802f
zorin <zorin@ZorinOS>
2014-02-06 22:56:52 -0600

    css menu y salir sesion

--- src/java/com/example/page/BorderPage.java
+++ src/java/com/example/page/BorderPage.java
@@ -4,6 +4,7 @@ import db.pojos.User;
 import manager.session.SessionController;
 import manager.session.Variable;
 import org.apache.click.Page;
+import org.apache.click.control.ActionLink;
 import org.apache.click.extras.control.Menu;
 import org.apache.click.extras.control.MenuFactory;
 
@@ -12,23 +13,13 @@ public abstract class BorderPage extends Page {
     public String userName;
     public String title;
     public String message;
+    public ActionLink logOut;
     private Menu rootMenu;
 
     public BorderPage() {
-        addMenu();
+        addCommonControls();
         init();
-        if (SessionController.getVariable("user") != null) {
-            Variable user = SessionController.getVariable("user");
-            if (user != null) {
-                userName = ((User) user.getValue()).getUser();
-            }
-        }
-        if (SessionController.getVariable("title") != null) {
-            Variable titlevar = SessionController.getVariable("title");
-            if (titlevar != null) {
-                title = (String) titlevar.getValue();
-            }
-        }
+        checkSessionVars();
     }
 
     /**
@@ -52,20 +43,45 @@ public abstract class BorderPage extends Page {
     }
 
     /**
-     * @return the title
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
      * agrega el Menu de inicio
      */
-    private void addMenu() {
+    private void addCommonControls() {
         MenuFactory menuFactory = new MenuFactory();
         rootMenu = menuFactory.getRootMenu();
         rootMenu.setName("rootMenu");
         rootMenu.setId("rootMenu");
         addControl(rootMenu);
+        
+        logOut=new ActionLink("logOut", "Salir",this, "onLogOut");
+        addControl(logOut);
+    }
+
+    /**
+     * evento que se lanza cuando se aprieta salir
+     * @return 
+     */
+    public boolean onLogOut(){
+        setRedirect(LoginPage.class);
+        SessionController.cleanMap();
+        return true;
+    }
+    
+    /**
+     * Verifica las variables de sesion y trae los respectivos valores
+     */
+    private void checkSessionVars() {
+
+        if (SessionController.getVariable("user") != null) {
+            Variable user = SessionController.getVariable("user");
+            if (user != null) {
+                userName = ((User) user.getValue()).getUser();
+            }
+        }
+        if (SessionController.getVariable("title") != null) {
+            Variable titlevar = SessionController.getVariable("title");
+            if (titlevar != null) {
+                title = (String) titlevar.getValue();
+            }
+        }
     }
 }
